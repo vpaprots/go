274b3c4e2ad6ae16e5e36b4cd92d6bff15ba1e45
Volodymyr Paprotski <vpaprots@ca.ibm.com>
2016-05-18 18:56:40 -0400

    quick, Invers() is done, dont sneeze while it works!

---
+++ src/crypto/elliptic/p256_asm_s390x.s
@@ -0,0 +1,339 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+#include "textflag.h"
+
+#define res_ptr R1
+#define x_ptr R2
+#define y_ptr R3
+#define X0    V0
+#define X1    V1
+#define Y0    V2
+#define Y1    V3
+#define M0    V4
+#define M1    V5
+#define T0    V6
+#define T1    V7
+#define T2    V8
+#define YDIG  V9
+
+#define ADD1  V16
+#define ADD1H V17
+#define ADD2  V18
+#define ADD2H V19
+#define RED1  V20
+#define RED1H V21
+#define RED2  V22
+#define RED2H V23
+#define CAR1  V24
+#define CAR1M V25
+
+
+#define MK0   V30
+#define K0    V31
+
+DATA p256ordK0<>+0x00(SB)/4, $0xee00bc4f
+DATA p256ord<>+0x00(SB)/8, $0xffffffff00000000
+DATA p256ord<>+0x08(SB)/8, $0xffffffffffffffff
+DATA p256ord<>+0x10(SB)/8, $0xbce6faada7179e84
+DATA p256ord<>+0x18(SB)/8, $0xf3b9cac2fc632551
+DATA p256one<>+0x00(SB)/8, $0x0000000000000001
+DATA p256one<>+0x08(SB)/8, $0xffffffff00000000
+DATA p256one<>+0x10(SB)/8, $0xffffffffffffffff
+DATA p256one<>+0x18(SB)/8, $0x00000000fffffffe
+GLOBL p256const0<>(SB), 8, $8
+GLOBL p256const1<>(SB), 8, $8
+GLOBL p256ordK0<>(SB), 8, $4
+GLOBL p256ord<>(SB), 8, $32
+GLOBL p256one<>(SB), 8, $32
+
+/* ---------------------------------------*/
+// func p256OrdMul(res, in1, in2 []uint64)
+TEXT ·p256OrdMul(SB),NOSPLIT,$0
+	MOVD res+0(FP), res_ptr
+	MOVD in1+24(FP), x_ptr
+	MOVD in2+48(FP), y_ptr
+
+	VZERO T2
+	MOVD	$p256ordK0<>+0x00(SB), R4
+	//VLEF    $3, 0(R4), K0
+	WORD	$0xE7F40000
+	BYTE    $0x38
+	BYTE    $0x03 //
+	MOVD	$p256ord<>+0x00(SB), R4
+	VL	16(R4), M0
+	VL	0(R4),  M1
+
+	VL (1*16)(x_ptr), X0
+	VL (0*16)(x_ptr), X1
+	VL (1*16)(y_ptr), Y0
+	VL (0*16)(y_ptr), Y1
+
+	//---------------------------------------------------------------------------/
+	VREPF $3,  Y0,   YDIG
+	VMLF  X0,  YDIG, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMLF  X1,YDIG, ADD2
+	VMLHF X0,YDIG, ADD1H
+	VMLHF X1,YDIG, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+	VREPF $2,  Y0,   YDIG
+	VMALF X0,  YDIG, T0, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMALF  X1,YDIG, T1, ADD2
+	VMALHF X0,YDIG, T0, ADD1H
+	VMALHF X1,YDIG, T1, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+	VREPF $1,  Y0,   YDIG
+	VMALF X0,  YDIG, T0, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMALF  X1,YDIG, T1, ADD2
+	VMALHF X0,YDIG, T0, ADD1H
+	VMALHF X1,YDIG, T1, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+	VREPF $0,  Y0,   YDIG
+	VMALF X0,  YDIG, T0, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMALF  X1,YDIG, T1, ADD2
+	VMALHF X0,YDIG, T0, ADD1H
+	VMALHF X1,YDIG, T1, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+	VREPF $3,  Y1,   YDIG
+	VMALF X0,  YDIG, T0, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMALF  X1,YDIG, T1, ADD2
+	VMALHF X0,YDIG, T0, ADD1H
+	VMALHF X1,YDIG, T1, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+	VREPF $2,  Y1,   YDIG
+	VMALF X0,  YDIG, T0, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMALF  X1,YDIG, T1, ADD2
+	VMALHF X0,YDIG, T0, ADD1H
+	VMALHF X1,YDIG, T1, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+	VREPF $1,  Y1,   YDIG
+	VMALF X0,  YDIG, T0, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMALF  X1,YDIG, T1, ADD2
+	VMALHF X0,YDIG, T0, ADD1H
+	VMALHF X1,YDIG, T1, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+	VREPF $0,  Y1,   YDIG
+	VMALF X0,  YDIG, T0, ADD1
+	VMLF  ADD1,K0,   MK0
+	VREPF $3,  MK0,  MK0
+
+	VMALF  X1,YDIG, T1, ADD2
+	VMALHF X0,YDIG, T0, ADD1H
+	VMALHF X1,YDIG, T1, ADD2H
+
+	VMALF  M0,MK0, ADD1, RED1
+	VMALHF M0,MK0, ADD1, RED1H
+	VMALF  M1,MK0, ADD2, RED2
+	VMALHF M1,MK0, ADD2, RED2H
+
+	VSLDB  $12, RED2,RED1, RED1
+	VSLDB  $12, T2,  RED2, RED2
+
+	VACCQ  RED1, ADD1H,CAR1
+	VAQ    RED1, ADD1H,T0
+	VACCQ  RED1H,T0,   CAR1M
+	VAQ    RED1H,T0,   T0
+
+	//<< ready for next MK0
+
+	VACQ    RED2, ADD2H,CAR1,  T1
+	VACCCQ  RED2, ADD2H,CAR1,  CAR1
+	VACCCQ  RED2H,T1, CAR1M, T2
+	VACQ    RED2H,T1, CAR1M, T1
+	VAQ     CAR1, T2,T2
+
+	//---------------------------------------------------
+
+	VZERO   RED1
+	VSCBIQ  M0,T0, CAR1
+	VSQ     M0,T0, ADD1
+	VSBCBIQ T1,M1,CAR1, CAR1M
+	VSBIQ   T1,M1,CAR1, ADD2
+	VSBIQ   T2,RED1,CAR1M, T2
+
+	//what output to use, ADD2||ADD1 or T1||T0?
+	VSEL    T0,ADD1,T2, T0
+	VSEL    T1,ADD2,T2, T1
+
+	VST T0, (1*16)(res_ptr)
+	VST T1, (0*16)(res_ptr)
+	RET
+
+
